b70cacfa9ddb38ef5ef65dd6c19c6e29e74cc505
hiroshishishi <364453hiroshi@gmail.com>
2020-01-15 14:16:58 +0900

    グループ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
-  resources :users, only: [:edit, :update, ]
+  root 'messages#index'
+  resources :messages, only: [:index]
+  resources :users, only: [:index, :edit, :update, ]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
